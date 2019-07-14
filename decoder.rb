7af9b9c1dacc5da1a21cb40739008a652cffe7f4
igor <igycol@yahoo.co.uk>
2019-07-14 19:26:28 +0200

    add some changes to the same file for the conflict to appear

--- decoder.rb
+++ decoder.rb
@@ -1,3 +1,5 @@
 this is a command first to appear when opening a file
 
 new line added to the third branch: add-letters
+
+adding some additional conversions to the file for merge conflict to appear
