83d86bf04587e8b33f57c89833e8489a99055cac
igor <igycol@yahoo.co.uk>
2019-07-15 13:18:14 +0200

    add z converger

--- decoder.rb
+++ decoder.rb
@@ -3,3 +3,5 @@ this is a command first to appear when opening a file
 fixes a bug in the program, we are only checking for the merging of branches
 
 impatient programer added some text
+
+26 => 'Z' change to commit in remote repo
